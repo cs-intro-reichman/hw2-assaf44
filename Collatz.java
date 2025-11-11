c4501efd1cdb29f8dc34e978e0078db4052a97d0
ASSAF44 <assaf.barel@post.runi.ac.il>
2025-11-12 00:12:21 +0200

    completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,40 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+    public static void main(String args[]){
+// step 1: recieving upperBound and mode
+        int upperBound = Integer.parseInt(args[0]);
+        String mode = args[1];
+// step 2: loop until reaching 1
+        for (int i = 1; i <= upperBound; i++){
+            int currentNumber = i;
+            int steps = 0;
+// step 3: mode v
+            if (mode.equals("v")){
+                System.out.print(i + " ");
+                while (currentNumber != 1){
+                    steps++;
+                    if (currentNumber % 2 == 0){
+                        currentNumber = currentNumber / 2;
+                    } else{
+                        currentNumber = (currentNumber * 3) + 1;
+                    }
+                    System.out.print(currentNumber + " ");
+                }
+                System.out.println("(" + steps + ")");
+            }
+// step 4: mode c
+            else if (mode.equals("c")){
+                while (currentNumber != 1){
+                    steps++;
+                    if (currentNumber % 2 == 0){
+                        currentNumber = currentNumber / 2;
+                    } else{
+                        currentNumber = (currentNumber * 3) + 1;
+                    }
+                }
+            }
+        }
+// stewp 5: mandatory printing
+        System.out.println("Every one of the first " + upperBound + " hailstone sequences reached 1.");
+    }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,36 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+// step 1: recieving information
+        String Rtime = (args[0]);
+        String minutesToAdd = (args[1]);
+// step 2: converting String to int
+        int hours = Integer.parseInt(Rtime.substring(0, 2));
+        int minutes = Integer.parseInt(Rtime.substring(3, 5));
+        int minutesToAddInt = Integer.parseInt(minutesToAdd);
+// step 3: converting hours to minutes and adding the sums
+        int tempTime = ((hours * 60) + minutes + minutesToAddInt);
+// step 4: converting back to formal time
+        int totalHours = (tempTime / 60);
+        int newHours = (totalHours % 12);
+        int newMinutes = (tempTime - (totalHours * 60));
+// step 5: attention to special cases
+        String printStringHours = String.valueOf(newHours);
+        String printH;
+            if (newHours < 10){
+            printH = ("0" + printStringHours);
+            }
+                else{
+                printH = printStringHours;
+                }
+        String printStringMinutes = String.valueOf(newMinutes);
+        String printM;
+            if (newMinutes == 0){
+            printM = ("0" + printStringMinutes);
+            }
+                else{
+                printM = printStringMinutes;
+                }
+// step 6: printing
+        System.out.println(printH + ":" + printM);
     }
 }
